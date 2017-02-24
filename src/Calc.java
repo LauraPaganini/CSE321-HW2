685943caba12c611f00699ec5bb21dfc513a01fb
Laura Paganini <Laura Paganini>
2017-02-24 15:35:28 -0500

    Q1 implementation

--- src/Calc.java
+++ src/Calc.java
@@ -9,4 +9,19 @@ public class Calc
    {
       return a + b;
    }
+   
+   static public int subtract (int a, int b)
+   {
+      return a - b;
+   }
+   
+   static public int multiply (int a, int b)
+   {
+      return a * b;
+   }
+   
+   static public int divide (int a, int b)
+   {
+      return a / b;
+   }
 }
