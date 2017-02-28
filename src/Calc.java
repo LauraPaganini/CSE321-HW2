c7f54e910f64568847f478dc0f8d70e5185104aa
Laura Paganini <Laura Paganini>
2017-02-27 21:09:32 -0500

    Revert "breaking the build"
    This reverts commit cfbc427931cbff4c77cb670d6ad6034024dcb6cc.

--- src/Calc.java
+++ src/Calc.java
@@ -1,9 +1,8 @@
+
 // Introduction to Software Testing
 // Authors: Paul Ammann & Jeff Offutt
 // Chapter 3; page ??
 // See CalcTest.java, DataDrivenCalcTest.java for JUnit tests
-//
-// Implemented by Laura Paganini
 
 public class Calc
 {
@@ -14,16 +13,16 @@ public class Calc
    
    static public int subtract (int a, int b)
    {
-      return a + b;
+      return a - b;
    }
    
    static public int multiply (int a, int b)
    {
-      return a + b;
+      return a * b;
    }
    
    static public int divide (int a, int b)
    {
-      return a + b;
+      return a / b;
    }
 }
